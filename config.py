a36498b9155fc74ca567806f418b985ad51c40c0
Devendra Bijwe <22devendrabijwe@gmail.com>
2023-04-15 13:43:00 +0000

    Replace config.py

--- config.py
+++ config.py
@@ -1,22 +1,15 @@
-# from __init__ import params
-
-from sqlalchemy import inspect
-
 from sqlalchemy_utils import database_exists, create_database
 import json
-with open("config.json","r") as c:
-    params=json.load(c)['params']
 
+with open("/var/www/MemerKida/MemerKida/config.json", "r") as c:
+    params = json.load(c)["params"]
 
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class Config(object) :
-    if (params['local_server']):
-    
-        SQLALCHEMY_DATABASE_URI=params['local_url']
+    if params["local_server"]:
+        SQLALCHEMY_DATABASE_URI = params["local_url"]
     else:
-        if not database_exists(params['prod_url']):
-            create_database(params['prod_url'])
-        SQLALCHEMY_DATABASE_URI=params['prod_url']
-    
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+        if not database_exists(params["prod_url"]):
+            create_database(params["prod_url"])
+        SQLALCHEMY_DATABASE_URI = params["prod_url"]
